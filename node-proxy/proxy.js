f7cff6169b219e8ccf6decf097d97fd9eb208008
Richard Ward <richard.ward@methods.co.uk>
2025-03-27 13:15:29 +0000

    Conditionally modify the host target of the proxy

--- node-proxy/proxy.js
+++ node-proxy/proxy.js
@@ -2,18 +2,41 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 
+const domainMappings = [
+  {
+    pattern: /^(?!v2)/,
+    targetDomain: "api.example.com",
+    protocol: "https"
+  }
+]
+
 const server = http.createServer((req, res) => {
   const target = url.parse(req.url);
+  let targetHostname = target.hostname;
+  let targetPort = target.port || 443;
+  let targetProtocol = "https";
+
+  for(const mapping of domainMappings) {
+    if (mapping.pattern.test(target.path)) {
+      targetHostname = mapping.targetDomain;
+      targetPort = mapping.port;
+      targetProtocol = mapping.protocol;
+      console.log(`Route overriden to: ${targetProtocol}://${targetHostname}:${targetPort} for path: ${target.path}`)
+      break;
+    }
+  }
+  
   const options = {
-    hostname: target.hostname,
-    port: target.port || 443,
+    hostname: targetHostname,
+    port: targetPort,
     path: target.path,
     method: req.method,
     headers: req.headers,
     // minVersion: 'TLSv1.3' // Force TLS 1.3
   };
 
-  const proxyReq = https.request(options, (proxyRes) => {
+  const requestHandler = targetProtocol === "https" ? https : http;
+  const proxyReq = requestHandler.request(options, (proxyRes) => {
     const dt = (new Date()).toJSON().slice(0,19).replace('T',':')
     console.log(`${dt} ${proxyRes.statusCode} ${proxyRes.req.method} ${proxyRes.req.host} ${proxyRes.req.path}`);
     res.writeHead(proxyRes.statusCode, proxyRes.headers);
