fc5665d2733679fbba5007378b8cba690de9e950
Lea Hayes <lea.hayes@education.gov.uk>
2025-03-27 15:11:10 +0000

    Debug connection issues.

--- node-proxy/proxy.js
+++ node-proxy/proxy.js
@@ -3,33 +3,47 @@ const https = require('https');
 const url = require('url');
 
 const domainMappings = [
+  // Reroute to public API in DEV environment.
   {
-    pattern: /^\/api\/v1\/.*$/,
-    targetDomain: "api.exmaple.com",
-    protocol: "https"
+    sourceDomain: "localhost",
+    sourcePort: 5086,
+    sourcePattern: /^services\/[^/]+\/organisations\/[^/]+\/users\/[^/]+$/,
+    targetDomain: "dev-api.signin.education.gov.uk",
+    targetProtocol: "https"
   }
 ]
 
 const server = http.createServer((req, res) => {
-  const target = url.parse(req.url);
-  let targetHostname = target.hostname;
-  let targetPort = target.port || 443;
+  const originalRequestUrl = req.url;
+  const requestedUrl = url.parse(originalRequestUrl);
+
+  let targetHostname = requestedUrl.hostname;
+  let targetPort = requestedUrl.port || 443;
   let targetProtocol = "https";
 
-  for(const mapping of domainMappings) {
-    if (mapping.pattern.test(target.path)) {
-      targetHostname = mapping.targetDomain;
-      targetPort = mapping.port;
-      targetProtocol = mapping.protocol;
-      console.log(`Route overriden to: ${targetProtocol}://${targetHostname}:${targetPort} for path: ${target.path}`)
+  // console.log(`Requesting: ${req.url}`);
+
+  for (const mapping of domainMappings) {
+    if (mapping.sourceDomain === requestedUrl.hostname &&
+        mapping.sourcePort === targetPort &&
+        mapping.sourcePattern.test(requestedUrl.path)
+    ) {
+      targetHostname = mapping.targetDomain ?? targetHostname;
+      targetPort = mapping.port ?? targetPort;
+      targetProtocol = mapping.protocol ?? targetProtocol;
+      console.log(`Route overriden to: ${targetProtocol}://${targetHostname}:${targetPort} for path: ${requestedUrl.path}`)
       break;
     }
+  }
+
+  if (targetHostname.includes("localhost")) {
+    targetProtocol = requestedUrl.protocol;
   }
   
   const options = {
     hostname: targetHostname,
     port: targetPort,
-    path: target.path,
+    path: requestedUrl.path,
     method: req.method,
     headers: req.headers,
     // minVersion: 'TLSv1.3' // Force TLS 1.3
@@ -38,13 +52,14 @@ const server = http.createServer((req, res) => {
   const requestHandler = targetProtocol === "https" ? https : http;
   const proxyReq = requestHandler.request(options, (proxyRes) => {
     const dt = (new Date()).toJSON().slice(0,19).replace('T',':')
-    console.log(`${dt} ${proxyRes.statusCode} ${proxyRes.req.method} ${proxyRes.req.host} ${proxyRes.req.path}`);
+    console.log(`${dt} ${proxyRes.req.method} ${originalRequestUrl}\n  --> ${proxyRes.statusCode} ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path}`);
     res.writeHead(proxyRes.statusCode, proxyRes.headers);
     proxyRes.pipe(res); // Plain HTTP back
   });
 
   req.pipe(proxyReq);
   proxyReq.on('error', (err) => {
+    console.error(err);
     res.writeHead(500);
     res.end('Proxy error: ' + err.message);
   });
