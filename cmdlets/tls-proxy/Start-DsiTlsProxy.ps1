da909e3b667accd6e1fd8e1347d5c2f702c2608b
Lea Hayes <lea.hayes@education.gov.uk>
2025-12-17 12:45:44 +0000

    Update documentation around `Start-DsiTlsProxy` since it's actually useful for Windows developers as well.

--- cmdlets/tls-proxy/Start-DsiTlsProxy.ps1
+++ cmdlets/tls-proxy/Start-DsiTlsProxy.ps1
@@ -1,9 +1,14 @@
 function Start-DsiTlsProxy {
     <#
     .SYNOPSIS
-        Start the TLS proxy for developers working on Mac.
+        Start the TLS proxy for developers when running components locally on a
+        development machine.
 
     .DESCRIPTION
+        This is required since the required version of TLS is not supported by
+        .NET Core 8.0 when running on a Mac development machine. Some developers
+        have also found this useful when running on Windows development machines.
+
         The proxy runs on http://localhost:8080
 
     .OUTPUTS
